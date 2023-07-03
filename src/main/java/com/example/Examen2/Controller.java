e4728d179594b63594b7ad658798a17511d7684d
Piero-Rodriguez-Cansaya <134323367+Piero-Rodriguez-Cansaya@users.noreply.github.com>
2023-07-03 17:51:14 +0000

    Update4

--- src/main/java/com/example/Examen2/Controller.java
+++ src/main/java/com/example/Examen2/Controller.java
@@ -6,16 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@RequestMapping(path="/")
+@RequestMapping(path="/idat")
 public class Controller{
 
     @GetMapping(path="/")
     public String home(){
         return "A18007010 - Alejandro";
     }
-    @GetMapping(path="/idat/codigo")
-    public String homes(){
+    @GetMapping(path="/codigo")
+    public String codigo(){
         return "A18007010";
+    }
+    @GetMapping(path="/Nombre-completo")
+    public String nombre(){
+        return "Alejandro Piero Rodriguez";
     }
 
 }
