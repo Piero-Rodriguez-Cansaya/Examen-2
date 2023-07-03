4d7d302543485774bbfa58c4f8e02ff40b3248ae
Piero-Rodriguez-Cansaya <134323367+Piero-Rodriguez-Cansaya@users.noreply.github.com>
2023-07-03 18:46:57 +0000

    Update5

---
+++ src/main/java/com/example/Examen2/User.java
@@ -0,0 +1,43 @@
+package com.example.Examen2;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+ import jakarta.persistence.Table;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "Curso")
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+
+	private String name;
+
+	private String credito;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCredito() {
+		return credito;
+	}
+
+	public void setCredito(String email) {
+		this.credito = credito;
+	}
+}
