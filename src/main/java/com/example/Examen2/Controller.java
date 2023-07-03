47ead61d37b1862d9b1bac5c9bf2cd049f6aaf58
Piero-Rodriguez-Cansaya <134323367+Piero-Rodriguez-Cansaya@users.noreply.github.com>
2023-07-03 17:02:44 +0000

    Update1

---
+++ src/main/java/com/example/Examen2/Controller.java
@@ -0,0 +1,17 @@
+package com.example.Examen2;
+
+import java.lang.String;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping(path="/")
+public class Controller{
+
+    @GetMapping(path="/")
+    public String home(){
+        return "A18007010 - Alejandro";
+    }
+
+}
